51ac9c75763345de42b99d1cad093b5b37275c83
Rohit Kumar <rkumarmehta922@gmail.com>
2025-04-30 10:32:59 +0530

    just for testing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,15 +18,11 @@ const Navbar = () => {
   }, []);
 
   return (
-    <div className="font-[Poppins] bg-gradient-to-t from-[#fbc2eb] to-[#a6c1ee] min-h-screen">
+    <div className="font-[Poppins] ">
       <header className="bg-white shadow-md">
-        <nav className="flex justify-between items-center w-[92%] mx-auto py-2">
+        <nav className="flex justify-between items-center w-[92%] mx-auto py-4">
           <div>
-            <img
-              className="w-16 cursor-pointer"
-              src="https://cdn-icons-png.flaticon.com/512/5968/5968204.png"
-              alt="Logo"
-            />
+            <h1 className='font-bold-700 text-lg'>NewsMannia.in</h1>
           </div>
           <div
             className={`nav-links duration-500 md:static absolute bg-white md:min-h-fit min-h-[60vh] left-0 ${
@@ -36,41 +32,40 @@ const Navbar = () => {
             <ul className="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8">
               <li>
                 <a className="hover:text-gray-500" href="#">
-                  Products
+                  Business
                 </a>
               </li>
               <li>
                 <a className="hover:text-gray-500" href="#">
-                  Solution
+                  Technology
                 </a>
               </li>
               <li>
                 <a className="hover:text-gray-500" href="#">
-                  Resource
+                  India
                 </a>
               </li>
               <li>
                 <a className="hover:text-gray-500" href="#">
-                  Developers
+                  Finance
                 </a>
               </li>
               <li>
                 <a className="hover:text-gray-500" href="#">
-                  Pricing
+                  Entertainment
                 </a>
               </li>
             </ul>
           </div>
           <div className="flex items-center gap-6">
-            <button className="bg-[#a6c1ee] text-white px-5 py-2 rounded-full hover:bg-[#87acec]">
-              Sign in
-            </button>
             <div className="text-3xl md:hidden cursor-pointer" onClick={toggleMenu}>
               {menuOpen ? <FiX /> : <FiMenu />}
             </div>
           </div>
         </nav>
       </header>
+
+      
     </div>
   );
 };
